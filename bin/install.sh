e760a4819b3f08f75135cf276ea00eca484f88f5
Francis Araujo <francis.araujo@gmail.com>
2023-07-16 17:39:20 -0300

    Update to install microk8s

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function updateKubeConfig {
+    echo "Update kube config in server..."
+    ssh ksys@192.168.15.101 "/snap/bin/microk8s.config > ~/.kube/config"
+
+    echo "Download kube config from server..."
+    scp ksys@192.168.15.101:~/.kube/config ~/.kube/config
+}
+
 ansible-galaxy install -f -r requirements.yml
 
 ansible-playbook -i ./inventory/cluster/hosts.ini ./install.yml
+
+if [ $? -eq 0 ]; then
+    updateKubeConfig
+fi
